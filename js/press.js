b4d69c5c2441a5efd6c7a8518856a32be58a9a0f
yunqianw <qianqian@yunqian-wangs-MacBook-Pro.local>
2013-11-27 08:58:37 -0500

    Kickstarter sample page
    123

--- js/press.js
+++ js/press.js
@@ -1,1 +1,46 @@
 press.js
+jquery(document).ready(function($){
+$(window).stellar();
+var links=$('.navigation').find('li');
+slide=$('.slide');
+mywindow=$(window);
+htmlbody=$('html.body');
+slide.waypoint(function (event,direction){
+
+	dataslide=$(this).attr('data-slide');
+		if(direction === 'down'){
+			$('.navigation li[data-slide="'+dataslide+'"]').addClass('active'
+
+		}
+
+		else{
+			$('.navigation li[data-slide="'+dataslide+'"]').addClass('active')
+
+		}
+});
+
+
+
+mywindow.scroll(function(){
+	if (mywindow.scrollTop()==0){
+		$('.navigation li[data-slide="1"]').addClass('active');
+		$('.navigation li[data-slide="2"]').removeClass('active');
+	}
+});
+
+function goToByScroll(dataslide){
+	htmlbody.animate({
+		scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutQuint');
+    }
+
+	links.click(function(e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+    //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
+   
+});
+
+}
